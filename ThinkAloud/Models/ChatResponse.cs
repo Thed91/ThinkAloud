434b844c8261f92295014ce1d339df6564545bab
Fiodar Mikhadziuk <f.mikhadziuk@gmail.com>
2026-01-30 18:39:09 +0400

    added backend logic

---
+++ ThinkAloud/Models/ChatResponse.cs
@@ -0,0 +1,8 @@
+namespace ThinkAloud.Models;
+
+public class ChatResponse
+{
+    public string Response { get; set; } = string.Empty;
+    public bool Success { get; set; }
+    public string? Error { get; set; }
+}
